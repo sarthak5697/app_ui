62842c9fa0c06cf52831b3986f93e6c6a7e8ea33
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2022-11-01 19:24:17 +0530

    first commit

---
+++ lib/home/main_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        child: Row(
+          children:[
+            Column(
+              children:[
+                
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
