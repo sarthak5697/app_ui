b4673457617c78471215c485e8343e372e832f06
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2022-11-02 10:48:45 +0530

    commit1

--- lib/home/main_page.dart
+++ lib/home/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/src/widgets/framework.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -12,16 +11,24 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
     return Container(
+      // ignore: avoid_unnecessary_containers
       child: Container(
         child: Row(
-          children:[
-            Column(
-              children:[
-                
-              ],
-            )
-          ],
+          children: [
+                Column(
+                  children: const [
+                    
+                  ],
+                ),
+                // ignore: sized_box_for_whitespace
+                Container(
+                  width: 45,
+                  height:45,
+
+                ),
+        ],
         ),
       ),
     );
