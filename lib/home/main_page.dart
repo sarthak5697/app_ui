9d2fe55ca3543a4591a1001ee5cafa6baa80932b
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2022-11-02 12:24:15 +0530

    commit2

--- lib/home/main_page.dart
+++ lib/home/main_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 // import 'package:flutter/src/widgets/framework.dart';
 
@@ -12,23 +14,74 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     // ignore: avoid_unnecessary_containers
-    return Container(
-      // ignore: avoid_unnecessary_containers
-      child: Container(
-        child: Row(
-          children: [
-                Column(
-                  children: const [
+
+    return Scaffold(
+      body: Container(
+        // ignore: avoid_unnecessary_containers
+        child: Container(
+          margin: const EdgeInsets.only(
+            top: 35,
+            bottom: 25,
+          ),
+          padding: const EdgeInsets.only(
+            left: 15,
+            right: 15,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      image: ,
+                  borderRadius: BorderRadius.circular(50),
+                  color: Color.fromARGB(255, 185, 185, 185),
+                ),
+                    height: 60,
+                    width: 60,
                     
+                  ),
+                  Text('   Hello , Piyush  ',
+                  ),
+                  Icon(
+                    Icons.waving_hand,
+                    color: Colors.yellow,
+                  ),
+                ],
+              ),
+              // ignore: sized_box_for_whitespace
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 5,
+                  bottom: 5,
+                ),
+                padding: const EdgeInsets.only(
+                  left: 5,
+                  right: 5,
+                ),
+                width: 70,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Color.fromARGB(255, 185, 185, 185),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    // ignore: prefer_const_constructors
+                    Icon(
+                      Icons.currency_rupee,
+                      color: Colors.yellow,
+                    ),
+                    const Text('1000'),
                   ],
                 ),
-                // ignore: sized_box_for_whitespace
-                Container(
-                  width: 45,
-                  height:45,
-
-                ),
-        ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_ui/home/main_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,11 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title:'Flutter Demo',
       theme:ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:const MyApp(),
+      home: const MainPage(),
     );
   }
 }
